b0cc0db9d40858dbbcea9bdadb82e9c1d6a733f0
Bruno Domingues <bruno_signorelli@outlook.com>
2021-05-31 21:21:44 -0300

    Small changes + added comments

--- summarizer.py
+++ summarizer.py
@@ -1,14 +1,15 @@
+import argparse
 import spacy
+import warnings
 from spacy.lang.pt.stop_words import STOP_WORDS as sw_pt
 from spacy.lang.en.stop_words import STOP_WORDS as sw_en
 from sklearn.feature_extraction.text import CountVectorizer
-import argparse
 
 
 def summarization(args):
     if args.lang == "PT":
         stopwords = sw_pt
-        
+
         if args.method == "eff":
             model = spacy.load("pt_core_news_sm")
 
@@ -16,11 +17,15 @@ def summarization(args):
             model = spacy.load("pt_core_news_lg")
 
         else:
-            print("Unsupported method")
+            raise ValueError("Unsupported method")
 
     elif args.lang == "EN":
+        warnings.filterwarnings(
+            "ignore",
+            message="Your stop_words may be inconsistent with your preprocessing.",
+        )
         stopwords = sw_en
-        
+
         if args.method == "eff":
             model = spacy.load("en_core_web_sm")
 
@@ -35,29 +40,36 @@ def summarization(args):
 
     text = str(input("Enter text: \n"))
 
+    # Carrega o texto com o modelo selecionado
     doc = model(text)
 
+    # Coloca os chars em minusculo
     text_lower = [sent.text.lower() for sent in doc.sents]
 
+    # CountVectorizer
     cv = CountVectorizer(stop_words=list(stopwords))
     cv_fit = cv.fit_transform(text_lower)
     word_list = cv.get_feature_names()
     count_list = cv_fit.toarray().sum(axis=0)
 
+    # Calcula a frequencia das palavras e ordena o dicionario pela frequecia
     word_frequency = dict(zip(word_list, count_list))
-
     val = sorted(word_frequency.values())
 
+    # Se most_common for True, mostra as palavras mais comuns
     if args.most_common:
-        higher_word_frequencies = [
+        most_frequent = [
             word for word, freq in word_frequency.items() if freq in val[-3:]
         ]
-        print(f"\nMost frequent words: {higher_word_frequencies}")
+        print(f"\nMost frequent words: {most_frequent}")
 
+    # Pega a maior frequencia e calcula as frequencias relativas a ela
     higher_frequency = val[-1]
+
     for word in word_frequency.keys():
         word_frequency[word] /= higher_frequency
 
+    # Faz o ranking das frases
     sentence_rank = {}
     for sent in doc.sents:
         for word in sent:
@@ -67,19 +79,17 @@ def summarization(args):
                 else:
                     sentence_rank[sent] = word_frequency[word.text.lower()]
 
-    top_sentences = sorted(sentence_rank.values())[::-1]
-    top_sent = top_sentences[: args.nsentences]
+    # Faz um sorting dos ranks de cada frase, coloca em ordem decrescente e depois seleciona as n frases
+    top_sent = sorted(sentence_rank.values())[::-1][: args.nsentences]
 
-    summary = []
-    for sent, strength in sentence_rank.items():
-        if strength in top_sent:
-            summary.append(sent)
+    # Gera o sumario
+    summary = [sent for sent, strength in sentence_rank.items() if strength in top_sent]
 
-    return text, summary
+    return summary
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Document summarizer")
+    parser = argparse.ArgumentParser(description="A simple document summarizer")
 
     parser.add_argument(
         "--nsentences",
@@ -102,9 +112,9 @@ if __name__ == "__main__":
         dest="method",
         default="eff",
         type=str,
-        help="Choose method: 'eff' (default, faster, but less acurate) or 'acc' (slower, but more accurate)",
+        help="Choose method for summarizing: 'eff' (default) (faster, but less acurate) or 'acc' (slower, but more accurate)",
     )
-    
+
     parser.add_argument(
         "--most_common",
         dest="most_common",
@@ -115,9 +125,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    text, summary = summarization(args)
+    summary = summarization(args)
 
     print("\n")
-    for i in summary:
-        print(i, end=" ")
+    for s in summary:
+        print(s, end=" ")
     print("\n")
